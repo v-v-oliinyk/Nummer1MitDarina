d8800a5ea2b40c7ce91ce869f2a824271b006ae2
Vadym Oliinyk <74012247+v-v-oliinyk@users.noreply.github.com>
2025-01-05 15:42:23 +0100

    Update passwordChecker.py

--- passwordChecker.py
+++ passwordChecker.py
@@ -9,18 +9,18 @@ def check_password(password):
     upper_case, lower_case, special_chars, digits, length = 0, 0, 0, 0, 0
 
     for char in password:
-        if len(password) > 16:
-            length += 1
-        elif char.isupper():
+        length = 1 if len(password) >= 16 else 0
+
+        if char.isupper():
             upper_case += 1
-        elif char.islower():
+        if char.islower():
             lower_case += 1
-        elif char.isdigit():
+        if char.isdigit():
             digits += 1
-        elif char in "₴#&_-@":
+        if char in "₴#&_-@!":
             special_chars += 1
 
-    if upper_case > 0 and lower_case > 0 and digits > 0 and length > 0 and special_chars > 0 :
+    if upper_case != 0 and lower_case != 0 and digits != 0 and length != 0 and special_chars != 0 :
         print("Password is enough strong!")
     else:
         if not len(password):
@@ -28,7 +28,7 @@ def check_password(password):
         if length == 0:
             print("Пароль має містити щонайменше 16 символів.")
         if special_chars == 0:
-            print("Пароль має містити щонайменше один спеціальний символ! (#&_-@)")
+            print("Пароль має містити щонайменше один спеціальний символ! (#&_-@!)")
         if digits == 0:
             print("Пароль має містити щонайменше одне число!")
         if upper_case == 0:
@@ -37,7 +37,7 @@ def check_password(password):
             print("Пароль має містити щонайменше одну маленьку літеру!")
 
 
-password_input = input(f"Будь ласка, введіть пароль що складається мінімум з 8 символів, містить мінімум одну маленьку, велику літеру, цифру та спеціальний символ (₴#&_-@): \n")
+password_input = input(f"Будь ласка, введіть пароль що складається мінімум з 8 символів, містить мінімум одну маленьку, велику літеру, цифру та спеціальний символ (₴#&_-@) : \n")
 check_password(password_input)
 
 #я так і не додумався як це реалізувати виключно через цикли.. треба якось оболонку змінити, а як...
